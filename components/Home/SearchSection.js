6f2059ba0bd7b1a166946fc4f51ac71d87abe26d
Ruben Servando <servandostudio@gmail.com>
2024-06-08 21:50:51 +0100

    Update search section

--- components/Home/SearchSection.js
+++ components/Home/SearchSection.js
@@ -8,6 +8,9 @@ function SearchSection() {
         <p className='text-[20px] font-bold'>Get a ride</p>
         <InputItem type='source' />
         <InputItem type='destination' />
+
+        <button className='p-3 bg-black w-full mt-5
+        text-white rounded-lg'>Search</button>
     </div>
   )
 }
